94468fd4d267ff5c36f793d408460657e8bf5c06
Tereza Pudilová <tery.pudilova@gmail.com>
2020-11-01 15:33:23 +0100

    Úprava navigace

--- src/index.js
+++ src/index.js
@@ -2,3 +2,25 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const btn = document.querySelector(".nav-btn");
+
+const navigace = document.querySelector(".nav")
+
+const pokrocilaNavigace = document.querySelectorAll(".menu-link")
+
+btn.addEventListener('click', (event) => {
+  if (navigace.className.includes("nav-closed")) {
+    navigace.className = "nav"
+  } else {
+    navigace.className = "nav nav-closed"
+  }
+}) 
+
+pokrocilaNavigace.forEach(item => {
+  item.addEventListener('click', (event) => {
+    navigace.className = "nav nav-closed"
+    console.log("vlivk")
+  }) 
+})
+
